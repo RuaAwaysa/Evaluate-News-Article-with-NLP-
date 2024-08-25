be2977dff4da8cc4e518f2436bf4a9e46c270870
RuaAwaysa <ruaana112001@gmail.com>
2024-08-26 00:05:23 +0300

    alien text api install

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,13 @@
 // js files
+
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
+
 import { handleSubmit } from './js/formHandler'
+import { checkForName } from './js/nameChecker'
 
 
 
