045e63ee5aaa3d273af52d31cf337376a0ae2e45
RuaAwaysa <ruaana112001@gmail.com>
2024-08-27 00:23:31 +0300

    meaningCloud Api config

--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,9 @@ var path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
+const aylien = require('aylien_textapi');
+const axios = require('axios');
+
 dotenv.config();
 
 const app = express();
@@ -12,13 +15,19 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static('dist'))
 console.log(__dirname);
-
+const API_KEY = process.env.API_KEY
+console.log(API_KEY)
 // Variables for url and api key
-
+// var textapi = new aylien({
+//     application_id: process.env.API_ID,
+//     application_key: process.env.API_KEY
+//   });
+  
 
 // app.get('/', function (req, res) {
 //     res.send("This is the server API page, you may access its services via the client app.");
 // });
+
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
     })
