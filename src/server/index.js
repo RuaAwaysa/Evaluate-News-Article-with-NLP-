fec2de548f79f7047b353acdc3a28388b1c29878
RuaAwaysa <ruaana112001@gmail.com>
2024-08-28 23:50:26 +0300

    some changes

--- src/server/index.js
+++ src/server/index.js
@@ -15,18 +15,15 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static('dist'))
 console.log(__dirname);
+
+
+// Variables for url and api key
 const API_KEY = process.env.API_KEY
 console.log(API_KEY)
-// Variables for url and api key
-// var textapi = new aylien({
-//     application_id: process.env.API_ID,
-//     application_key: process.env.API_KEY
-//   });
-  
 
-// app.get('/', function (req, res) {
-//     res.send("This is the server API page, you may access its services via the client app.");
-// });
+app.get('/', function (req, res) {
+    res.send('index.html');
+});
 
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
